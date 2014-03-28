0b518d6c3dd2b80177765cce02368ed7d6addec5
Antonin Januska <antonin@antjanus.com>
2014-03-27 21:03:26 -0500

    Updated thumbnail sizing

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ register_nav_menus(
 //post thumbnails
 if ( function_exists( 'add_theme_support' ) ) {
   add_theme_support( 'post-thumbnails' );
-  set_post_thumbnail_size(630, 300, true);
+  set_post_thumbnail_size(150, 150, true);
 }
 
 
