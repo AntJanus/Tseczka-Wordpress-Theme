791a977da0bc7df5e777f22f002c0b17d31402c5
Antonin Januska <antonin@antjanus.com>
2014-04-11 08:20:20 -0500

    Typography updates, widget, and styling updates

--- functions/sidebars.php
+++ functions/sidebars.php
@@ -1,29 +1,29 @@
 <?php
 // SIDEBARS
 register_sidebar(array('name'=>'footer-intro',
-'before_widget' => '',
-'after_widget' => '',
+'before_widget' => '<div class="widget">',
+'after_widget' => '</div>',
 'before_title' => '<h3>',
 'after_title' => '</h3>',
 ));
 
 register_sidebar(array('name'=>'footer-left',
-'before_widget' => '',
-'after_widget' => '',
+'before_widget' => '<div class="widget">',
+'after_widget' => '</div>',
 'before_title' => '<h3>',
 'after_title' => '</h3>',
 ));
 register_sidebar(array('name'=>'footer-mid',
-'before_widget' => '',
-'after_widget' => '',
+'before_widget' => '<div class="widget">',
+'after_widget' => '</div>',
 'before_title' => '<h3>',
 'after_title' => '</h3>',
 
 ));
 
 register_sidebar(array('name'=>'footer-end',
-'before_widget' => '',
-'after_widget' => '',
+'before_widget' => '<div class="widget">',
+'after_widget' => '</div>',
 'before_title' => '<h3>',
 'after_title' => '</h3>',
 
@@ -32,8 +32,8 @@ register_sidebar(array('name'=>'footer-end',
 for($i = 1; $i <= SIDEBAR_COUNT; $i++) {
     register_sidebar(array(
         'name'=>'sidebar-'.$i,
-        'before_widget' => '',
-        'after_widget' => '',
+        'before_widget' => '<div class="widget">',
+        'after_widget' => '</div>',
         'before_title' => '<h3>',
         'after_title' => '</h3>',
     ));
