ebe0b2073893fd479ef7f5f55f00c3179e3bdb15
Antonin Januska <antonin@antjanus.com>
2014-05-20 08:43:45 -0500

    Fixed front-page and JS directory

--- front-page.php
+++ front-page.php
@@ -1,25 +1,35 @@
 <?php get_header();?>
 
-<section id="mainContent" class="row"><!-- MAIN CONTENT WRAPPER -->
+<section id="main-content" class="row">
+  <section id="content-posts" class="large-8 columns feed-list">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <article id="post-<?php the_ID();?>" <?php post_class('post single feed-item');?> itemscope itemtype="http://schema.org/Article">
+        <h2 class="entry-title item fn"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>" class="url"><?php the_title(); ?></a></h2>
 
-  <section id="contentPosts" class="large-12 columns"><!-- content posts -->
+        <ul class="list-meta author vcard" itemscope itemtype="http://schema.org/Person" itemprop="author">
+          <li><time itemprop="datePublished" class="updated" datetime="<?php $postDate = get_the_date('c'); $postDate2 = get_the_date('d.m.Y'); echo $postDate ?>" pubdate><?php echo $postDate2; ?></time></li>
+          <li>Author: <span itemprop="name"> <?php the_author_posts_link(); ?></span></li>
+          <li>Categories: <?php the_category(', '); ?></li>
+        </ul>
 
-    <!-- START OF POST -->
-    <article id="post-<?php the_ID();?>" <?php post_class('post hentry hnews single');?>>
+        <div class="post-content feed-content entry-content">
+          <?php
+          if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+            the_post_thumbnail('thumbnail', array('class' => 'feed-thumb', 'itemprop' => 'image'));
+          }
+          the_excerpt();
+          ?>
+        </div>
 
+      </article>
+  <?php endwhile; endif;?>
+    <div class="pagination">
+      <?php posts_nav_link( ' &#183 ', 'previous page &raquo;', ' &laquo; next page' ); ?>
+    </div>
+  </section>
 
-      <div class="postContent entry-content">
-        <?php the_content(); ?>
-
-      </div>
-    </article>
-    <!-- END OF POST -->
-  <div id="blog-nav">
-    <?php posts_nav_link( ' &#183 ', 'previous page &raquo;', ' &laquo; next page' ); ?>
-  </div>
+  <?php get_sidebar();?>
 </section>
-</section>
-<!-- END MAIN CONTENT WRAPPER -->
 
 <?php get_footer();?>
 <?php wp_footer();?>
